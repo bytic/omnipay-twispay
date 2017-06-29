8a9e63935cccb307ab6780f00e6e51e3c02c4417
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-06-29 12:19:22 +0300

    final fixes for purchase request

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -9,6 +9,17 @@ use ByTIC\Omnipay\Twispay\Helper;
  */
 class PurchaseRequest extends AbstractRequest
 {
+
+    /**
+     * @inheritdoc
+     */
+    public function initialize(array $parameters = [])
+    {
+        $parameters['orderType'] = isset($parameters['orderType']) ? $parameters['orderType'] : 'purchase';
+        $parameters['identifier'] = isset($parameters['identifier']) ? $parameters['identifier'] : 'anonymous';
+        return parent::initialize($parameters);
+    }
+
     /**
      * @inheritdoc
      */
@@ -16,6 +27,7 @@ class PurchaseRequest extends AbstractRequest
     {
         $data = $this->getData();
         $data['checksum'] = Helper::generateChecksun($data, $this->getApiKey());
+        $data['redirectUrl'] = $this->getSecureUrl();
 
         return $this->response = new PurchaseResponse($this, $data, $this->getSecureUrl());
     }
@@ -32,15 +44,28 @@ class PurchaseRequest extends AbstractRequest
         );
 
         $data = [
+//            'apiKey' => $this->getSiteId(),
+//            'notifyUrl' => $this->getNotifyUrl(),
             'siteId' => $this->getSiteId(),
+            'backUrl' => $this->getReturnUrl(),
             'identifier' => $this->getIdentifier(),
             'amount' => $this->getAmount(),
             'currency' => $this->getCurrency(),
             'description' => $this->getDescription(),
             'orderType' => $this->getOrderType(),
             'orderId' => $this->getOrderId(),
-            'checksum' => $this->getChecksum(),
+//            'checksum' => $this->getChecksum(),
         ];
+
+        $card = $this->getCard();
+
+        $data['firstName'] = $card->getBillingFirstName();
+        $data['lastName'] = $card->getBillingLastName();
+//        $data['address'] = $card->getBillingAddress1();
+        $data['phone'] = $card->getBillingPhone();
+        $data['email'] = $card->getEmail();
+        $data['invoiceEmail'] = $card->getEmail();
+
         return $data;
     }
 
