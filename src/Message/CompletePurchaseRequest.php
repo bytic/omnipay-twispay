aed1694e48ba4e83d9e7b68fd7579cb3cd30ccc6
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-07-12 21:51:17 +0300

    add check for both result and opensslResult in complete request and server complete request

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -2,8 +2,7 @@
 
 namespace ByTIC\Omnipay\Twispay\Message;
 
-use ByTIC\Omnipay\Common\Message\Traits\GatewayNotificationRequestTrait;
-use ByTIC\Omnipay\Twispay\Helper;
+use ByTIC\Omnipay\Twispay\Message\Traits\CompletePurchaseRequestTrait;
 
 /**
  * Class PurchaseResponse
@@ -11,23 +10,5 @@ use ByTIC\Omnipay\Twispay\Helper;
  */
 class CompletePurchaseRequest extends AbstractRequest
 {
-    use GatewayNotificationRequestTrait;
-
-    /**
-     * @return bool|mixed
-     */
-    protected function parseNotification()
-    {
-        $json = Helper::decrypt($this->httpRequest->request->get('opensslResult'), $this->getApiKey());
-        $data = json_decode($json, true);
-        return $data;
-    }
-
-    /**
-     * @return mixed
-     */
-    protected function isValidNotification()
-    {
-        return $this->hasGet('id') && $this->hasPOST('opensslResult');
-    }
+    use CompletePurchaseRequestTrait;
 }
